c266446fa8c2258b90c62381fcdc23c64132075b
wusiliang <844615849@qq.com>
2021-07-08 17:54:44 +0800

    master

--- src/main/java/com/atguigu/git/gitTest.java
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -9,6 +9,7 @@ public class gitTest {
         System.out.println("master update!");
         System.out.println("hot fix!");
         System.out.println("master test!");
+        System.out.println("master test!");
     }
 
 }
